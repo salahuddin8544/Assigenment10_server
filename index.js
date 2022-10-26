2946bdee0c3ca663fb71b8e092a6a77963519148
salahuddin8544 <salahuddinahmed8544@gmail.com>
2022-10-26 19:28:28 +0600

    create file

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app= express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+const categories = require('./data/categories.json');
+const courses = require('./data/courses.json')
+app.get('/',(req,res)=>{
+    res.send('Running api')
+})
+
+app.get('/categories',(req,res)=>{
+    res.send(categories)
+})
+app.get('/course-all',(req,res)=>{
+    res.send(courses)
+})
+app.listen(port,()=>{
+    console.log('port is running');
+})
